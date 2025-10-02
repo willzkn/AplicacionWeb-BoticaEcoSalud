7b23a13b76f6911c700dea6880b41407b39c0189
willzkn <grekelzkn@gmail.com>
2025-10-01 23:35:17 -0500

    Punto de retorno

--- botica-backend/src/main/java/com/botica/botica_backend/WebConfig.java
+++ botica-backend/src/main/java/com/botica/botica_backend/WebConfig.java
@@ -13,13 +13,12 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        //.allowedOrigins("*")
                         .allowedOrigins("http://localhost:3000")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
             }
 
         };
     }
 }
-
-
